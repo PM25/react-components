0f9f6248f4193471fa54ce8cd79ad6841adf0903
PY <pyhuang97@gmail.com>
2022-12-23 22:22:50 +0800

    remove copyright.

--- src/footer.js
+++ src/footer.js
@@ -5,7 +5,6 @@ import "./css/footer.css";
 export default function Footer() {
     return (
         <div id="footer">
-            <div>Copyright © PlusMore 2018-2021</div>
             <div>
                 <span>Created & Designed by </span>
                 <a href="https://pm25.github.io">黃品硯 (Pin-Yen Huang)</a>
